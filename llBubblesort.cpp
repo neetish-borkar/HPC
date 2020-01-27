bd61eefbd4d92cc46e42d10d224f1c97d5c6e793
Neetish <57806399+neetish-borkar@users.noreply.github.com>
2020-01-28 02:04:43 +0530

    Add files via upload

---
+++ llBubblesort.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <cstdlib>
+#include <omp.h>
+#include <time.h>
+using namespace std;
+
+class sort{
+
+public:
+    int *array;
+    int  size;
+    int *array2;
+    void accept(int);
+    void display();
+    void display2();
+    void bubb_sort_ll();
+    void bubble_sort();
+    void check();
+
+};
+void sort::accept(int s){
+    size=s;
+    array=new int [size];
+    array2=new int [size];
+    for (int i=0;i<size;i++){
+        int x=rand()%size;
+        array[i]=x;
+        array2[i]=x;
+        
+    }
+}
+
+void sort::check(){
+    for(int i=0;i<size;i++){
+        if(array[i]!=array2[i]){
+            cout<<"worng"<<endl;
+            break;
+        }
+    }
+}
+
+void sort::display(){
+    for(int i=0; i<size;i++)
+    {
+        cout<<array[i]<<endl;
+    }
+}
+
+void sort::display2(){
+    for(int i=0; i<size;i++)
+    {
+        cout<<array2[i]<<endl;
+    }
+}
+
+void sort::bubb_sort_ll(){
+    int temp;
+
+	for (int i = 0; i < size/2; i++)
+	{
+		#pragma omp parallel for private(temp)  
+		for (int j = 0; j < size-1; j+=2) 
+		{
+			if (array[j]>array[j+1])
+			{
+				temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+			}
+		}
+		#pragma omp parallel for private(temp)  
+		for (int j = 1; j < size-1; j+=2) 
+		{
+			if (array[j]>array[j+1])
+			{
+				temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+			}
+		}	
+	}
+}
+void sort::bubble_sort(){
+    int temp;
+
+    for(int i=0;i<size-1;i++){
+
+        for(int j=0;j<size-i-1;j++){
+        
+            if(array2[j]>array2[j+1]){
+                temp=array2[j];
+				array2[j]=array2[j+1];
+				array2[j+1]=temp;
+            }
+        }
+    }
+
+}
+
+
+int main(){
+    int s;
+    double wtime;
+    double wtime2;
+    cout<<"Enter the length of array"<<endl;
+    cin>>s;
+    sort a;
+
+
+    wtime=omp_get_wtime();
+    a.accept(s);    
+    cout<<"Initialized Array"<<endl;
+    //a.display();
+    wtime2=omp_get_wtime()-wtime;
+
+
+    double wtime3=omp_get_wtime();
+    cout<<"Prallel Sorted Array"<<endl;
+    a.bubb_sort_ll();
+    //a.display();
+    double wtime4=omp_get_wtime()-wtime3;
+
+
+    double wtime5=omp_get_wtime();
+    cout<<"Normal Sorted Array"<<endl;
+    a.bubble_sort();
+    //a.display2();
+    double wtime6=omp_get_wtime()-wtime5;
+
+    a.check();
+
+    cout<<"Size="<<a.size<<endl;
+    cout<<"Time taken to initialize Array="<<wtime2<<"s"<<endl;
+    cout<<"Time taken to Prallel Bubble Sort Array="<<wtime4<<"s"<<endl;
+    cout<<"Time taken to Bubble Sort Array="<<wtime6<<"s"<<endl;
+}
+/*
+g++ -fopenmp llBubblesort.cpp
+Enter the length of array
+10000
+Initialized Array
+Prallel Sorted Array
+Normal Sorted Array
+Size=10000
+Time taken to initialize Array=0.000734287s
+Time taken to Prallel Bubble Sort Array=0.0898749s
+Time taken to Bubble Sort Array=0.350319s
+*/
---
+++ llquick_sort.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<cstdlib>
+#include<omp.h>
+using namespace std;
+
+long int partition(long int arr[],long int low,long int high){
+    long int pivot=arr[high];
+    long int temp;
+    long int i = low-1;
+    for (long int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    i++;
+    temp=arr[i];
+    arr[i]=arr[high];
+    arr[high]=temp;
+    return i;
+}
+
+
+long int quick_sort_ll(long int arr[],long int low,long int high){
+    if(low<high){
+        long int mid = partition(arr,low,high);
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                quick_sort_ll(arr,low,mid-1);
+            }
+            #pragma omp section
+            {
+            quick_sort_ll(arr,mid+1,high);
+            }
+        }
+
+    }
+}
+long int quick_sort(long int arr[],long int low,long int high){
+    if(low<high){
+        long int mid = partition(arr,low,high);
+        quick_sort(arr,low,mid-1);
+        quick_sort(arr,mid+1,high);
+    }
+}
+
+int main(){
+    long int n;
+    cout<<"Enter the number of elements:"<<endl;
+    cin>>n;
+
+    long int arr[n];
+    cout<<"Inserting elements"<<endl;
+    for(long int i=0;i<n;i++){
+        arr[i]=(rand()%1000000);
+    }
+    cout<<"Insertion completed"<<endl;
+    cout<<"Sorting elements with parallel Quick Sort"<<endl;
+    double starttime;
+    starttime = omp_get_wtime();
+    quick_sort_ll(arr,0,n-1);
+    double endtime = omp_get_wtime();
+    cout<<"Parallel Quick Sorting completed"<<endl;
+
+    cout<<"Sorting elements with Serial Quick Sort"<<endl;
+    double starttime2;
+    starttime2 = omp_get_wtime();
+    quick_sort(arr,0,n-1);
+    double endtime2 = omp_get_wtime();
+    cout<<"Serial Quick Sorting completed"<<endl;
+
+    cout << "Time Parallel: " << (endtime - starttime);
+    cout << "\nTime serial: " << (endtime2 - starttime2);
+
+    return 0;
+}
+/*
+Enter the number of elements:
+12000
+Inserting elements
+Insertion completed
+Sorting elements with parallel Quick Sort
+Parallel Quick Sorting completed
+Sorting elements with Serial Quick Sort
+Serial Quick Sorting completed
+Time Parallel: 0.0206267
+Time serial: 0.269715
+*/
